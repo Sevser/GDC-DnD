88fdd18d38f23c435c89c4c91251771e4278bd21
sevser <sevser39@gmail.com>
2023-02-09 23:16:56 +0300

    add rules page

---
+++ packages/web/src/components/rules/RuleContent.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card class="mb-4">
+    <v-card-item>
+      <v-card-title :id="section.index">{{ section.name }}</v-card-title>
+    </v-card-item>
+
+    <v-card-text>
+      {{ section.desc }}
+    </v-card-text>
+  </v-card>
+</template>
+<script lang="ts">
+import { RuleSectionModel } from '@/types/RuleSection/RuleSection';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    section: {
+      type: RuleSectionModel,
+      required: true,
+      default: () => RuleSectionModel.getEmpty(),
+    },
+  },
+});
+</script>
--- packages/web/src/pages/RulesView.vue
+++ packages/web/src/pages/RulesView.vue
@@ -1,20 +1,35 @@
 <template>
   <default-layout :title="title">
-    <div class="pl-2 pr-2" style="height: 100%">
+    <div class="pl-2 pr-2 pt-4" style="height: 100%">
       <div v-if="false" class="d-flex justify-center align-center" style="height: 100%">
         <v-progress-circular indeterminate :size="60" />
       </div>
-      <template v-else> {{ rule }} </template>
+      <template v-else>
+        <v-row class="h-100 w-100">
+          <v-col :cols="$vuetify.display.xs ? 0 : 3" class="d-none d-sm-flex flex-column align-center pt-6 h-100">
+            <RuleNavigationItem v-for="section in rule?.ruleSections" :key="section.index" :section="section" />
+          </v-col>
+          <v-col :cols="$vuetify.display.xs ? 12 : 9" class="d-flex flex-column h-100 overflow-y-auto pt-6" ref="ruleContainer">
+            <div>
+              <RuleContent v-for="section in rule?.ruleSections" :key="section.index" :section="section" />
+            </div>
+          </v-col>
+        </v-row>
+      </template>
     </div>
   </default-layout>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, VueElement } from 'vue';
 import DefaultLayout from '@/layout/default/DefaultLayout.vue';
+import RuleNavigationItem from '@/components/rules/RuleNavigationItem.vue';
+import RuleContent from '@/components/rules/RuleContent.vue';
 
 export default defineComponent({
   components: {
     DefaultLayout,
+    RuleNavigationItem,
+    RuleContent,
   },
   created() {
     this.$watch(
@@ -38,8 +53,17 @@ export default defineComponent({
       return this.$store.state.rules.currentRuleViewPending && this.rule === undefined;
     },
   },
+  methods: {
+    handleScroll() {
+      console.log('scroll');
+    },
+  },
   beforeUnmount() {
     this.$store.commit('rules/updateRuleView');
+    (this.$refs.ruleContainer as VueElement).$el.removeEventListener('scroll', this.handleScroll.bind(this));
+  },
+  mounted() {
+    (this.$refs.ruleContainer as VueElement).$el.addEventListener('scroll', this.handleScroll.bind(this));
   },
 });
 </script>
